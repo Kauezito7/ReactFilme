d43c8bc9939ef53ab57ca35d3ceeecd03d50cc5f
Kaue Moura <Kauehago07@gmail.com>
2025-04-22 16:47:56 -0300

    feat: atualizando

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
+import CadastroFilme from './pages/cadastroFilme/CadastroFilme.jsx'
+// import Login from './pages/login/Login.jsx';
+// Responsavel pela criacao de telas
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* <Login/> */}
+    <CadastroFilme/>
+    </>
+
   );
 }
-
+// serve para exportar uma única entidade (classe, função, objeto, etc.) de um módulo como a exportação padrão
 export default App;
