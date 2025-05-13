d247a2245ac15c7abe83e171914bba01df9174c3
Kaue Moura <Kauehago07@gmail.com>
2025-05-13 16:51:57 -0300

    feat: atualizando para implementacao da API

--- src/components/cadastro/Cadastro.jsx
+++ src/components/cadastro/Cadastro.jsx
@@ -4,13 +4,17 @@ import Botao from "../botao/Botao"
 const Cadastro = (props) => {
     return(
         <section className="section_cadastro">
-            <form action="" className="layout_grid form_cadastro">
+            <form onSubmit={props.funcCadastro} className="layout_grid form_cadastro">
             <h1>{props.titulo}</h1>
             <hr/>
             <div className="campos_cadastro">
                 <div className="campo_cad_nome">
                     <label htmlFor="nome">Nome:</label>
-                    <input type="text"  name="nome" placeholder={`Digite o nome do ${props.placeholder}`} />
+                    <input type="text"  name="nome" placeholder={`Digite o nome do ${props.placeholder}`} value={props.valorInput}
+                    // ao mudar o input algo acontece: 
+                    // Atualizar o estado do pai ao digitar 
+                    // taregt esta indo buscar o valor do "e"//
+                    onChange={(e) => props.setValorInput(e.target.value)} />
                 </div>
                 <div className="campo_cad_genero" style={{display:props.visibilidade}}>
                     <label htmlFor="genero">Genero:</label>
--- src/pages/cadastroGenero/CadastroGenero.jsx
+++ src/pages/cadastroGenero/CadastroGenero.jsx
@@ -1,23 +1,100 @@
+import { useState } from "react";
+import api from "../../Services/services";
 
+//Importar o sweet alert:
+import Swal from 'sweetalert2'
+
+// import { useEffect } from "react";
+
+// importacao de componentes:
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer"
 import Cadastro from "../../components/cadastro/Cadastro";
 import Lista from "../../components/lista/Lista";
 
 
+const CadastroGenero = () => {
 
-const  CadastroGenero = () => {
-    return(
-        <>
-        <Header/>
-        <main>
-          <Cadastro titulo = "Cadastro de Genero"  visibilidade = "none" placeholder = "Genero"/>
-          
-          <Lista titulo = "Lista de Genero" visibilidade = "none"/>
-        </main>
-        <Footer/>
-        </>
-      
-    )
+  //nome do genero
+  const [genero, setGenero] = useState("");
+
+  function alerta(icone, mensagem){
+    const Toast = Swal.mixin({
+  toast: true,
+  position: "top-end",
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.onmouseenter = Swal.stopTimer;
+    toast.onmouseleave = Swal.resumeTimer;
+  }
+});
+Toast.fire({
+  icon: icone,
+  title: mensagem
+});
+  }
+
+  // vai acontecer depois que eu clicar no botao cadastrar
+  async function cadastrarGenero(e) {
+    e.preventDefault();
+    //Verificar se o input esta vindo vazio
+    if (genero.trim() != "") {
+      //try => tentar(o esperado)
+      //catch => pega a exceção
+      try {
+        //cadastrar um gênero: post
+        await api.post("genero", { nome: genero })
+        alerta("success", "Cadastro realizado com sucesso!")
+        setGenero("")
+      }
+      catch (error) {
+        alerta("error", "Erro! Entre em contato com o suporte!")
+        console.log(error);
+       }
+    }
+    else {
+      alert("error", "Erro! Preencha o campo")
+    }
+
+  }
+
+
+
+  // //Teste
+  // useEffect(() => {
+  //   console.log(genero);
+  // },[genero]);
+  // //Fim do teste
+
+  return (
+    <>
+      <Header />
+      <main>
+        <Cadastro titulo="Cadastro de Genero"
+          visibilidade="none"
+          placeholder="Genero"
+
+
+          //Atribuindo a funcao:
+          funcCadastro={cadastrarGenero}
+
+          //Atribuindo o valor ao input:
+          valorInput={genero}
+
+          //Atribuindo a funcao que atualiza meu genero:
+          setValorInput={setGenero}
+        />
+
+        <Lista titulo="Lista de Genero"
+          visibilidade="none"
+        />
+
+      </main>
+      <Footer />
+    </>
+
+  )
 }
 export default CadastroGenero;
